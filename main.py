26824b613b84d4a986cf7933b5f82d467df53d88
Vasthavdas <vdasari@cbu.edu>
2025-04-23 18:06:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Allow all origins for testing purposes
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 # Load model
